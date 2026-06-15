82a2f7283b182abda63a3bc02206ef7c4df69e40
DariaAleshina <ddaleshina@gmail.com>
2026-06-15 19:07:53 +0200

    import hooks on respective pages

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,9 @@
+import { useDashboard } from '../mocked/hooks/useDashboard';
+
 export default function Home() {
+  const { dashboard, isLoading, error, refetch } = useDashboard();
+
+  console.log('isLoading: ', isLoading);
+  console.log('Dashboard data: ', dashboard);
   return null;
 }
--- app/routes/users.tsx
+++ app/routes/users.tsx
@@ -1,3 +1,9 @@
+import { useEmployees } from '../mocked/hooks/useEmployees';
+
 export default function Users() {
+  const { employees, isLoading, error, refetch } = useEmployees();
+
+  console.log('isLoading: ', isLoading);
+  console.log('Employees data: ', employees);
   return null;
 }
