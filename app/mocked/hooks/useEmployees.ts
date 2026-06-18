df81964507f9f215c7c4897e89a1ab7a2fe40b9f
DariaAleshina <ddaleshina@gmail.com>
2026-06-19 00:27:45 +0200

    fix type error

--- app/mocked/hooks/useEmployees.ts
+++ app/mocked/hooks/useEmployees.ts
@@ -8,7 +8,7 @@ import {
 
 type UseEmployeesResult = {
   employees: Employee[];
-  filters: EmployeeFilters | null;
+  filters: EmployeeFilters;
   isLoading: boolean;
   error: Error | null;
   refetch: () => void;
